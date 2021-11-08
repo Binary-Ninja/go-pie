b7c08a5c39033090a2e32c6e10e65bc411adc814
Binary-Ninja <91568729+Binary-Ninja@users.noreply.github.com>
2021-11-08 13:10:04 -0700

    Added Widget class.

--- scenes.py
+++ scenes.py
@@ -17,8 +17,18 @@ class NextScene(Enum):
     START = auto()
     HOST = auto()
     JOIN = auto()
-    GAME_HOST = auto()
-    GAME_JOIN = auto()
+    GAME = auto()
+
+
+class Widget:
+    """A basic widget that holds an image and a rectangle."""
+    def __init__(self, image: pg.Surface):
+        self.image = image
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen: pg.Surface):
+        """Draws the widget on the given surface."""
+        screen.blit(self.image, self.rect)
 
 
 class BaseScene:
@@ -55,25 +65,22 @@ class StartScene(BaseScene):
     """
     def __init__(self, screen_rect):
         super().__init__(screen_rect)
-        # Create the button images.
-        self.host_button_img = DEFAULT_FONT.render("(H)ost Server", True, BLACK, GRAY)
-        self.join_button_img = DEFAULT_FONT.render("(J)oin Server", True, BLACK, GRAY)
-        # Get the button rectangles.
-        self.host_button_rect = self.host_button_img.get_rect()
-        self.join_button_rect = self.join_button_img.get_rect()
-        # Place the buttons on the screen.
-        self.place_buttons()
+        # Create the widgets.
+        self.host_button = Widget(DEFAULT_FONT.render("(H)ost Server", True, BLACK, GRAY))
+        self.join_button = Widget(DEFAULT_FONT.render("(J)oin Server", True, BLACK, GRAY))
+        # Position the widgets on the screen.
+        self.position_widgets()
 
-    def place_buttons(self):
-        """Places the buttons in their proper place on screen."""
-        self.host_button_rect.centerx = self.screen_rect.centerx
-        self.join_button_rect.centerx = self.screen_rect.centerx
-        self.host_button_rect.centery = self.screen_rect.centery - self.host_button_rect.height - 10
-        self.join_button_rect.centery = self.screen_rect.centery + 10
+    def position_widgets(self):
+        """Places the widgets in their proper place on screen."""
+        self.host_button.rect.centerx = self.screen_rect.centerx
+        self.join_button.rect.centerx = self.screen_rect.centerx
+        self.host_button.rect.centery = self.screen_rect.centery - self.host_button.rect.height - 10
+        self.join_button.rect.centery = self.screen_rect.centery + 10
 
     def update_screen_size(self, screen_rect):
         self.screen_rect = screen_rect
-        self.place_buttons()
+        self.position_widgets()
 
     def events(self, events):
         for event in events:
@@ -88,14 +95,14 @@ class StartScene(BaseScene):
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     # Check for button clicks.
-                    if self.host_button_rect.collidepoint(event.pos):
+                    if self.host_button.rect.collidepoint(event.pos):
                         self.next_scene = NextScene.HOST
-                    elif self.join_button_rect.collidepoint(event.pos):
+                    elif self.join_button.rect.collidepoint(event.pos):
                         self.next_scene = NextScene.JOIN
 
     def draw(self, screen):
-        screen.blit(self.host_button_img, self.host_button_rect)
-        screen.blit(self.join_button_img, self.join_button_rect)
+        self.host_button.draw(screen)
+        self.join_button.draw(screen)
 
 
 class HostScene(BaseScene):
@@ -105,45 +112,42 @@ class HostScene(BaseScene):
     """
     def __init__(self, screen_rect):
         super().__init__(screen_rect)
-        # Create the button images.
-        self.start_button_img = DEFAULT_FONT.render("(ENTER) Start Server", True, BLACK, GRAY)
-        self.back_button_img = DEFAULT_FONT.render("(ESC) Main Menu", True, BLACK, GRAY)
-        # Get the button rectangles.
-        self.start_button_rect = self.start_button_img.get_rect()
-        self.back_button_rect = self.back_button_img.get_rect()
-        # Place the buttons on the screen.
-        self.place_buttons()
+        # Create the widgets.
+        self.start_button = Widget(DEFAULT_FONT.render("(ENTER) Start Server", True, BLACK, GRAY))
+        self.back_button = Widget(DEFAULT_FONT.render("(ESC) Main Menu", True, BLACK, GRAY))
+        # Position the widgets on the screen.
+        self.position_widgets()
 
-    def place_buttons(self):
-        """Places the buttons in their proper place on screen."""
-        self.start_button_rect.centery = self.screen_rect.centery
-        self.back_button_rect.centery = self.screen_rect.centery
-        self.start_button_rect.x = self.screen_rect.centerx + 10
-        self.back_button_rect.x = self.screen_rect.centerx - self.back_button_rect.width - 10
+    def position_widgets(self):
+        """Places the widgets in their proper place on screen."""
+        self.start_button.rect.centery = self.screen_rect.centery
+        self.back_button.rect.centery = self.screen_rect.centery
+        self.start_button.rect.x = self.screen_rect.centerx + 10
+        self.back_button.rect.x = self.screen_rect.centerx - self.back_button.rect.width - 10
 
     def update_screen_size(self, screen_rect):
         self.screen_rect = screen_rect
-        self.place_buttons()
+        self.position_widgets()
 
     def events(self, events):
         for event in events:
             if event.type == pg.KEYDOWN:
                 # Check for key commands.
                 if event.key == pg.K_RETURN:
-                    self.next_scene = NextScene.GAME_HOST
+                    self.next_scene = NextScene.GAME
                 elif event.key == pg.K_ESCAPE:
                     self.next_scene = NextScene.START
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     # Check for button clicks.
-                    if self.start_button_rect.collidepoint(event.pos):
-                        self.next_scene = NextScene.GAME_HOST
-                    elif self.back_button_rect.collidepoint(event.pos):
+                    if self.start_button.rect.collidepoint(event.pos):
+                        self.next_scene = NextScene.GAME
+                    elif self.back_button.rect.collidepoint(event.pos):
                         self.next_scene = NextScene.START
 
     def draw(self, screen):
-        screen.blit(self.start_button_img, self.start_button_rect)
-        screen.blit(self.back_button_img, self.back_button_rect)
+        self.start_button.draw(screen)
+        self.back_button.draw(screen)
 
 
 class JoinScene(BaseScene):
@@ -153,48 +157,43 @@ class JoinScene(BaseScene):
     """
     def __init__(self, screen_rect):
         super().__init__(screen_rect)
-        # Create the widget images.
-        self.start_button_img = DEFAULT_FONT.render("(ENTER) Join Server", True, BLACK, GRAY)
-        self.back_button_img = DEFAULT_FONT.render("(ESC) Main Menu", True, BLACK, GRAY)
+        # Create the widgets.
+        self.start_button = Widget(DEFAULT_FONT.render("(ENTER) Join Server", True, BLACK, GRAY))
+        self.back_button = Widget(DEFAULT_FONT.render("(ESC) Main Menu", True, BLACK, GRAY))
         # Create the text box.
         self.text = f"{DEFAULT_HOST}:{DEFAULT_PORT}"
-        self.text_box_img = DEFAULT_FONT.render(self.text, True, BLACK, GRAY)
-        # Get the widget rectangles.
-        self.start_button_rect = self.start_button_img.get_rect()
-        self.back_button_rect = self.back_button_img.get_rect()
-        self.text_box_rect = self.text_box_img.get_rect()
-        # Place the widgets on the screen.
-        self.place_buttons()
+        self.text_box = Widget(DEFAULT_FONT.render("Address: " + self.text, True, BLACK, GRAY))
+        # Position the widgets on the screen.
+        self.position_widgets()
 
-    def place_buttons(self):
-        """Places the buttons in their proper place on screen."""
+    def position_widgets(self):
+        """Places the widgets in their proper place on screen."""
         # Place the buttons.
-        self.start_button_rect.centery = self.screen_rect.centery
-        self.back_button_rect.centery = self.screen_rect.centery
-        self.start_button_rect.x = self.screen_rect.centerx + 10
-        self.back_button_rect.x = self.screen_rect.centerx - self.back_button_rect.width - 10
+        self.start_button.rect.centery = self.screen_rect.centery
+        self.back_button.rect.centery = self.screen_rect.centery
+        self.start_button.rect.x = self.screen_rect.centerx + 10
+        self.back_button.rect.x = self.screen_rect.centerx - self.back_button.rect.width - 10
         # Place the text box.
-        self.text_box_rect.centerx = self.screen_rect.centerx
-        self.text_box_rect.bottom = self.screen_rect.centery - self.start_button_rect.height
+        self.text_box.rect.centerx = self.screen_rect.centerx
+        self.text_box.rect.bottom = self.screen_rect.centery - self.start_button.rect.height
 
     def update_text_box(self):
         """Updates the text box after text has been changed."""
-        self.text_box_img = DEFAULT_FONT.render(self.text, True, BLACK, GRAY)
-        self.text_box_rect = self.text_box_img.get_rect()
-        # Place the text box.
-        self.text_box_rect.centerx = self.screen_rect.centerx
-        self.text_box_rect.bottom = self.screen_rect.centery - self.start_button_rect.height
+        self.text_box = Widget(DEFAULT_FONT.render("Address: " + self.text, True, BLACK, GRAY))
+        # Position the text box.
+        self.text_box.rect.centerx = self.screen_rect.centerx
+        self.text_box.rect.bottom = self.screen_rect.centery - self.start_button.rect.height
 
     def update_screen_size(self, screen_rect):
         self.screen_rect = screen_rect
-        self.place_buttons()
+        self.position_widgets()
 
     def events(self, events):
         for event in events:
             if event.type == pg.KEYDOWN:
                 # Check for key commands.
                 if event.key == pg.K_RETURN:
-                    self.next_scene = NextScene.GAME_JOIN
+                    self.next_scene = NextScene.GAME
                 elif event.key == pg.K_ESCAPE:
                     self.next_scene = NextScene.START
                 elif event.key == pg.K_BACKSPACE:
@@ -213,12 +212,12 @@ class JoinScene(BaseScene):
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     # Check for button clicks.
-                    if self.start_button_rect.collidepoint(event.pos):
-                        self.next_scene = NextScene.GAME_JOIN
-                    elif self.back_button_rect.collidepoint(event.pos):
+                    if self.start_button.rect.collidepoint(event.pos):
+                        self.next_scene = NextScene.GAME
+                    elif self.back_button.rect.collidepoint(event.pos):
                         self.next_scene = NextScene.START
 
     def draw(self, screen):
-        screen.blit(self.start_button_img, self.start_button_rect)
-        screen.blit(self.back_button_img, self.back_button_rect)
-        screen.blit(self.text_box_img, self.text_box_rect)
+        self.start_button.draw(screen)
+        self.back_button.draw(screen)
+        self.text_box.draw(screen)
